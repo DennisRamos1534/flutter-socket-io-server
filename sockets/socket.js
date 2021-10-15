1926fab8d9b761fe82dad91689adaa500e97b336
Dennis Ramos <dsrg1995@gmail.com>
2021-10-15 17:44:27 -0500

    Cambios bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands'); 
+
+const bands = new Bands();
+
+bands.addBand(new Band('Angeles Azules'));
+bands.addBand(new Band('Rock'));
+bands.addBand(new Band('Pop'));
+bands.addBand(new Band('Metalica'));
+
+console.log(bands);
 
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -10,7 +23,26 @@ io.on('connection', client => {
 
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
-
         io.emit('mensaje', {admin: 'Nuevo Mensaje'});
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('crear-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    //     // console.log(payload);
+    // });
 });
